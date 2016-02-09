7363c34c599c1fb55928d9ec0601c97d3c3bb46d
Adar Shaikhiev <a.shaikhiev@innopolis.ru>
2016-02-09 20:11:34 +0300

    Merger updated

--- System A/MergerFilter.java
+++ System A/MergerFilter.java
@@ -19,34 +19,29 @@ public class MergerFilter extends SystemFilter{
 
     public void run() {
         //int portCount = getNumberOfOpenedInputPorts();
-        Frame lastAFrame = null;
-        Frame lastBFrame = null;
-        try {
+        Frame lastAFrame;
+        Frame lastBFrame;
+        lastAFrame = inputPortIsAlive(0) ? this.readCurrentFrame(0) : null;
+        lastBFrame = inputPortIsAlive(1) ? this.readCurrentFrame(1) : null;
 
-            lastAFrame = inputPortIsAlive(0) ? this.readCurrentFrame(0) : null;
-            lastBFrame = inputPortIsAlive(1) ? this.readCurrentFrame(1) : null;
+        while(lastAFrame!=null && lastBFrame!=null){
 
-            while(lastAFrame!=null && lastBFrame!=null){
-
-                if(lastAFrame.getData().get(0) <= lastBFrame.getData().get(0)){
-                    System.out.println("\nframe: " + lastAFrame.getData());
-                    transmitCurrentFrame(lastAFrame);
-                    lastAFrame = readCurrentFrame(0);
-                }else {
-                    System.out.println("\nframe: " + lastBFrame.getData());
-                    transmitCurrentFrame(lastBFrame);
-                    lastBFrame = readCurrentFrame(1);
-                }
-            }
-        } catch (Exception e) {
-            if(lastBFrame != null){
-                transmitWholeStream(1);
+            if(lastAFrame.getData().get(0) <= lastBFrame.getData().get(0)){
+                System.out.println("\nframe: " + lastAFrame.getData());
+                transmitCurrentFrame(lastAFrame);
+                lastAFrame = readCurrentFrame(0);
             }else {
-                if(lastAFrame != null){
-                    transmitWholeStream(0);
-                }
+                System.out.println("\nframe: " + lastBFrame.getData());
+                transmitCurrentFrame(lastBFrame);
+                lastBFrame = readCurrentFrame(1);
             }
-            System.out.println("\n" + this.getName() + " MergerFilter:Merging error::" + e);
+        }
+        if(lastBFrame != null){
+            transmitWholeStream(1);
+        }else {
+            if(lastAFrame != null){
+                transmitWholeStream(0);
+            }
         }
     }
 }
