ccfb377c902a9da0ea397a2df660f54dc99f867b
atrantin <anton@mm94.ru>
2016-02-09 23:52:00 +0300

    fix

--- System A/Plumber.java
+++ System A/Plumber.java
@@ -26,47 +26,29 @@ public class Plumber {
         SourceFilter source = new SourceFilter();
         TemperatureFilter temperature = new TemperatureFilter();
         AttitudeFilter attitude = new AttitudeFilter();
-        SinkFilter sink = new SinkFilter();
+        SystemASink sink = new SystemASink();
         FrameFilter frameFilter = new FrameFilter();
-
-        SourceFilter sourceA = new SourceFilter("SubSetA.dat");
-        SourceFilter sourceB = new SourceFilter("SubSetB.dat");
-        MergerFilter merger = new MergerFilter();
 
         /****************************************************************************
          * Here we connect the filters starting with the sink filter (filter1) which we connect to
          * filter2 the middle filter. Then we connect Filter2 to the source filter (filter3).
          ****************************************************************************/
-        //System A test
 
-        /*
+        //System A test
         sink.connect(attitude); // This esstially says, "connect sink input port to attitude output port
         attitude.connect(temperature); // This esstially says, "connect attitude input port to temperature output port
         temperature.connect(frameFilter); // This esstially says, "connect temperature intput port to source output port
         frameFilter.connect(source);
-        */
-
-        //System C test
-        merger.connect(sourceA);
-        merger.connect(sourceB);
-        sink.connect(merger);
 
         /****************************************************************************
          * Here we start the filters up. All-in-all,... its really kind of boring.
          ****************************************************************************/
         //System A start
-        /*
         source.start();
         frameFilter.start();
         temperature.start();
         attitude.start();
         sink.start();
-        */
 
-        //System C start
-        sourceA.start();
-        sourceB.start();
-        merger.start();
-        sink.start();
     } // main
 } // Plumber
